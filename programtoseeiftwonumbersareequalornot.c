2c81b78a1fa7a823a8a01042d39095a1b1c74774
arc-hit890 <25114cn350@glbitm.ac.in>
2026-02-12 10:22:06 +0530

    Add files via upload

---
+++ programtoseeiftwonumbersareequalornot.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter two numbers\n");
+    scandf("%d %d", &a, &b);
+    if (a==b);
+    {
+        printf("Both numbers are equal");
+    }
+    else;
+    {
+        printf("Both nummbers are not equal");
+    }
+    return 0;
+}
