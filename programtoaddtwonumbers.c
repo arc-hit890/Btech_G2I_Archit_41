7e9f8f8c9304c71bd929b28f48b861049fd674f5
arc-hit890 <25114cn350@glbitm.ac.in>
2026-02-12 10:01:39 +0530

    Add files via upload

---
+++ programtoaddtwonumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x, y, z;
+    printf("I have created a new Github account today\n");
+    printf("Enter the value x ");
+    scanf("%d", &x);
+    printf("Enter the value of y");
+    scanf("%d", &y);
+    z = x + y;
+    printf("The sum of x and y is %d", z);
+    return 0;
+}
